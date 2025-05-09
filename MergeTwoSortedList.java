8233a5a8ea76f98b6c15c0a103da5a9631c8876f
Dhrumil Patel <138426097+dhrumilpatel09@users.noreply.github.com>
2025-05-09 11:09:28 +0530

    Solved MergeTwoSortedList.java

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -1,10 +1,10 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Create a dummy node to form the base of the merged list
+        
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
 
-        // Traverse both lists and add the smaller node to the result
+        
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
                 current.next = list1;
@@ -16,7 +16,7 @@ class Solution {
             current = current.next;
         }
 
-        // Attach the remaining elements of either list
+        
         if (list1 != null) {
             current.next = list1;
         } else {
